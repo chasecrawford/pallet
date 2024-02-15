4f678d8d7e0f45a198da372f73a613226c7a08b6
CHASE CRAWFORD <chasecrawford@hatfieldmedia.com>
2024-02-15 15:28:57 -0500

    another sp fix

--- src/Providers/PalletServiceProvider.php
+++ src/Providers/PalletServiceProvider.php
@@ -8,12 +8,12 @@ class PalletServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
 
         $this->publishes([
-            __DIR__.'/../resources/css' => resource_path('css/vendor/pallet'),
-            __DIR__.'/../resources/js' => resource_path('js/vendor/pallet'),
-            __DIR__.'/../resources/views' => resource_path('views/vendor/pallet'),
+            __DIR__.'/../../resources/css' => resource_path('css/vendor/pallet'),
+            __DIR__.'/../../resources/js' => resource_path('js/vendor/pallet'),
+            __DIR__.'/../../resources/views' => resource_path('views/vendor/pallet'),
         ]);
 
     }
